aa72fc0dddd969d6621dc524c76ad8a6511da10e
Ruan Vitor Cordeiro da Silva <ruan59333@gmail.com>
2023-07-19 16:24:06 -0300

    models: unidade basica, tipo, estado e resultado de teste

--- ifnotifica_serv/settings.py
+++ ifnotifica_serv/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
     'api',
     'web'
 ]
@@ -47,7 +46,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- web/models.py
+++ web/models.py
@@ -1,4 +1,3 @@
-from random import choice
 from django.db import models
 
 class Estado(models.TextChoices):
@@ -38,6 +37,25 @@ class Cor(models.TextChoices):
     BRANCO = "BRAN", "Branco"
     PARDO = "PARD", "Pardo"
     AMARELO = "AMARELO", "Amarelo"
+    PRETO = "PRETO", "Preto"
+    INDIGINA = "INDIGINA", "Indígina"
+    IGNORADO = "IGNORADO", "Ignorado"
+
+class TipoTeste(models.TextChoices):
+    RT_QPCR = "RT-QCPR", "Teste molecular em tempo real - RT-PCR"
+    RT_LAMP = "RT-LAMP", "Teste molecular de amplianção isotermica - RT-LAMP"
+    TESTE_RAPIDO = "ANTIGENO", "Teste rapido com Aitígeno"
+ 
+class EstadoTeste(models.TextChoices):
+    SOLICITADO = "SOLICITADO", "Teste solicitado"
+    COLETADO = "COLETADO", "Teste coletado"
+    CONCLUIDO = "CONCLUIDO", "Teste concluido" 
+    NAO_SOLICITADO = "NAO_SOLICITADO", "Teste não solicitado"
+
+class Resultado(models.TextChoices):
+    REAGENTE = "REAGENTE", "Resultado positivo"
+    NAO_REAGENTE = "NAO_REAGENTE", "Resultado negativo"
+    INDETERMINADO = "INDETERMINADO", "Resultado indeterminado" 
 
 class Cidade(models.Model):
     descricao = models.CharField(verbose_name="Cidade", blank=False, null=False, max_length=255)
@@ -55,51 +73,55 @@ class Bairro(models.Model):
 
 class UnidadeBasica(models.Model):
     #TODO: Criar entidade par ubs, toda ubs tem descricao e cidade
-    pass
+    descricao = models.CharField(verbose_name="Unidade basica", blank=False, null=False, max_length=128)
+    cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT)
 
-# Create your models here.
-# Create your models here.
+    def __str__(self):
+        return f"{self.descricao}, {self.cidade}"
+
+
 class Paciente(models.Model):
     id_paciente = models.AutoField(primary_key=True)
-    cpf = models.CharField(max_length=11)
-    cbo = models.CharField(max_length=15)
-    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11, blank=False, null=False)
+    cbo = models.CharField(max_length=15, blank=True, null=False)
+    nome = models.CharField(max_length=100, blank=False, null=False)
     data_nascimento = models.DateField()
     sexo = models.CharField(max_length=10, choices=Sexo.choices)
     cor = models.CharField(max_length=20, choices = Cor.choices)
-    tradicionalidade = models.BooleanField()
-    cep = models.CharField(max_length=8)
-    logradouro = models.CharField(max_length=100)
+    tradicionalidade = models.BooleanField(blank=False, null=False)
+    cep = models.CharField(max_length=8, blank=False, null=False)
+    logradouro = models.CharField(max_length=100, blank=True, null=False)
     numero = models.CharField(max_length=10)
     complemento = models.CharField(max_length=100)
     bairro = models.ForeignKey(Bairro, on_delete=models.PROTECT)
-    telefone1 = models.CharField(max_length=15)
-    telefone2 = models.CharField(max_length=15)
-    email = models.CharField(max_length=100)
+    telefone1 = models.CharField(max_length=15, blank=False, null=False)
+    telefone2 = models.CharField(max_length=15, blank=False, null=False)
+    email = models.EmailField(max_length=100, blank=False, null=False)
     data_cadastro = models.DateField()
     
     def __str__(self) -> str:
         return f"{self.cpf}, {self.nome}"
 
 class Atendimento(models.Model):
-  estrategia_atendimento = models.CharField(max_length=100)
-  local = models.CharField(max_length=100) #TODO: mudar local_atendimento para chave estrangeiro com ubs
-  paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
-  data_cadastro = models.DateField()
+    estrategia_atendimento = models.CharField(max_length=100, blank=False, null=False)
+    local = models.ForeignKey(UnidadeBasica, on_delete=models.PROTECT)  #TODO: mudar local_atendimento para chave estrangeiro com ubs
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    data_cadastro = models.DateField(blank=False, null=False)
   
-  def __str__(self) -> str:
+    def __str__(self) -> str:
         return f"{self.paciente}, {self.local}"
     
 class Notificacao(models.Model):
-  data_notificacao = models.DateField()
-  tipo_teste = models.CharField(max_length=100) #TODO: fazer como textchoice (dando as opcoes validas)
-  estado_teste = models.CharField(max_length=100) #TODO: fazer como textchoice (dando as opcoes validas)
-  assintomatico = models.BooleanField()
-  sintomas = models.CharField(max_length=100)
-  condicoes_especiais = models.CharField(max_length=100)
-  atendimento = models.ForeignKey(Atendimento, on_delete=models.CASCADE)
-  data_cadastro = models.DateField()
-  data_envio = models.DateField()
+    data_notificacao = models.DateField()
+    tipo_teste = models.CharField(max_length=100) #TODO: fazer como textchoice (dando as opcoes validas)
+    estado_teste = models.CharField(max_length=100) #TODO: fazer como textchoice (dando as opcoes validas)
+    resultado = models.CharField(max_length=100) #TODO: fazer como textchoice (dando as opcoes validas)
+    assintomatico = models.BooleanField()
+    sintomas = models.CharField(max_length=100)
+    condicoes_especiais = models.CharField(max_length=100)
+    atendimento = models.ForeignKey(Atendimento, on_delete=models.CASCADE)
+    data_cadastro = models.DateField()
+    data_envio = models.DateField()
 
-  def __str__(self) -> str:
+    def __str__(self) -> str:
         return f"{self.atendimento.paciente}, {self.data_cadastro}"
