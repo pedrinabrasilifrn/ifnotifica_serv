1f67f32828259a60074d14c02b13759dfe5dceb9
Ruan Cordeiro <ruan59333@gmail.com>
2023-09-13 17:42:48 -0300

    graficos

--- web/views.py
+++ web/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Count
 import plotly.express as px
+import plotly.graph_objects as go
 from web.models import Notificacao, UnidadeBasica 
 import calendar
 
@@ -27,22 +28,52 @@ def index(request):
         numero_de_notificacoes_por_mes = px.bar(
             x = [calendar.month_name[notificacao.data_cadastro.month] for notificacao in notificacoes],
             y = [1 for notificacao in notificacoes],
-            labels= {'x':'Mês', 'y':'Notificações cadastradas'},
-            title='Número de notificações por mês',
+            labels = {'x':'Mês', 'y':'Notificações cadastradas'},
+            color= [notificacao.resultado for notificacao in notificacoes],
+            title ='Número de notificações por mês',
         )
 
         numero_de_notificacoes_por_sexo = px.bar(
             x = [notificacao.atendimento.paciente.sexo for notificacao in notificacoes],
-            y = [1 for notificacao in notificacoes],
-            labels= {'x':'Sexo', 'y':'Notificações enviadas'},
-            title='Número de notificações por sexo',
+            labels = {'x':'Sexo', 'y':'Notificações enviadas'},
+            color= [notificacao.atendimento.paciente.sexo for notificacao in notificacoes],
+            title ='Número de notificações por sexo',
+        )
+
+        numero_de_notificacos_positivas_por_estado = px.bar(
+            x = [notificacao.atendimento.paciente.cidade.estado for notificacao in notificacoes if notificacao.resultado == 'REAGENTE'],
+            y = [1 for notificacao in notificacoes if notificacao.resultado == 'REAGENTE'],
+            labels = {'x':'Estado', 'y':'Notificações positivas'},
+            color = [notificacao.atendimento.paciente.cidade.estado for notificacao in notificacoes if notificacao.resultado == 'REAGENTE'],
+            title ='Número de notificações positivas por estado',
+        )
+
+        # TODO: ajeitar histograma
+        numero_de_notificacoes_por_idade_e_sexo = px.histogram(
+            # TODO: Calcular a idade do paciente
+            y = [notificacao.atendimento.paciente.sexo for notificacao in notificacoes],
+            labels = {'x':'Idade', 'y':'Notificações enviadas'},
+            color = [notificacao.atendimento.paciente.sexo for notificacao in notificacoes],
+            title ='Número de notificações por idade e sexo',
+        )
+
+        # TODO: ajeitar barra
+        numero_de_notificacoes_com_tal_teste = px.bar(
+            x = [notificacao.tipo_teste for notificacao in notificacoes],
+            labels = {'x':'Teste', 'y':'Notificações enviadas'},
+            color = [notificacao.tipo_teste for notificacao in notificacoes],
+            title ='Número de notificações por teste',
         )
 
         grafico1 = numero_de_notificacoes_por_mes.to_html()
         grafico2 = numero_de_notificacoes_por_sexo.to_html()
+        grafico3 = numero_de_notificacos_positivas_por_estado.to_html()
+        grafico4 = numero_de_notificacoes_por_idade_e_sexo.to_html()
 
         context["grafico1"] =  grafico1
         context["grafico2"] = grafico2
+        context["grafico3"] = grafico3
+        context["grafico4"] = grafico4
         context["ultima_atualizacao"] =  ultima_atualizacao
 
     return render(request, 'web/index.html', context=context)
