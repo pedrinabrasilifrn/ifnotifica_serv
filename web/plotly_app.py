491f66b6e3cc5ac522f052c30917d878bb05580b
Ruan Cordeiro <ruan59333@gmail.com>
2023-09-21 15:13:27 -0300

    tabela + dataframe funcionando

--- web/plotly_app.py
+++ web/plotly_app.py
@@ -1,18 +1,36 @@
 from dash import Dash, dash_table, dcc, html, Input, Output, callback
 import pandas as pd
 from django_plotly_dash import DjangoDash
+from web.models import Notificacao
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+notificacoes = Notificacao.objects.all()
+df_tabela = pd.DataFrame(columns=['notificacao', 'sexo', 'clinica', 'cidade', 'estado', 'tipo de teste', 'resultado', 'data de notificação'])
 
-app = DjangoDash('SimpleExample')
+# collect all the data for this dataframe that is referred in the columns above
+for n in notificacoes:
+    notificacao_atual = n.id
+    sexo = n.atendimento.paciente.sexo
+    clinica = n.atendimento.local.descricao
+    cidade = n.atendimento.local.cidade.descricao
+    estado = n.atendimento.local.cidade.estado
+    tipo_de_teste = n.tipo_teste
+    resultado = n.resultado
+    data_de_notificacao = n.data_envio
+
+    # create a dictionary with the data collected above
+    data = {'notificacao': notificacao_atual, 'sexo': sexo, 'clinica': clinica, 'cidade': cidade, 'estado': estado, 'tipo de teste': tipo_de_teste, 'resultado': resultado, 'data de notificação': data_de_notificacao}
+
+    # append the dictionary to the dataframe
+    df_tabela.loc[len(df_tabela)] = data
+
+
+app = DjangoDash('tabela_notificacoes')
 
 app.layout = html.Div([
     dash_table.DataTable(
-        id='datatable-interactivity',
-        columns=[
-            {"name": i, "id": i, "selectable": True} for i in df.columns
-        ],
-        data=df.to_dict('records'),
+        id='tabela_notificacoes',
+        columns=[{"name": i, "id": i} for i in df_tabela.columns],
+        data=df_tabela.to_dict('records'),
         filter_action="native",
         sort_action="native",
         sort_mode="multi",
@@ -21,68 +39,23 @@ app.layout = html.Div([
         selected_rows=[],
         page_action="native",
         page_current= 0,
-        page_size= 20,
+        page_size= 5,
+        style_table={
+            'width': '100%',
+            'margin': 'auto',
+        },
     ),
     html.Div(id='datatable-interactivity-container')
-])
+    ],
+)
 
 @callback(
     Output('datatable-interactivity', 'style_data_conditional'),
     Input('datatable-interactivity', 'selected_columns')
 )
+
 def update_styles(selected_columns):
     return [{
         'if': { 'column_id': i },
         'background_color': '#D2F3FF'
     } for i in selected_columns]
-
-@callback(
-    Output('datatable-interactivity-container', "children"),
-    Input('datatable-interactivity', "derived_virtual_data"),
-    Input('datatable-interactivity', "derived_virtual_selected_rows"))
-def update_graphs(rows, derived_virtual_selected_rows):
-    # When the table is first rendered, `derived_virtual_data` and
-    # `derived_virtual_selected_rows` will be `None`. This is due to an
-    # idiosyncrasy in Dash (unsupplied properties are always None and Dash
-    # calls the dependent callbacks when the component is first rendered).
-    # So, if `rows` is `None`, then the component was just rendered
-    # and its value will be the same as the component's dataframe.
-    # Instead of setting `None` in here, you could also set
-    # `derived_virtual_data=df.to_rows('dict')` when you initialize
-    # the component.
-    if derived_virtual_selected_rows is None:
-        derived_virtual_selected_rows = []
-
-    dff = df if rows is None else pd.DataFrame(rows)
-
-    colors = ['#7FDBFF' if i in derived_virtual_selected_rows else '#0074D9'
-              for i in range(len(dff))]
-
-    return [
-        dcc.Graph(
-            id=column,
-            figure={
-                "data": [
-                    {
-                        "x": dff["country"],
-                        "y": dff[column],
-                        "type": "bar",
-                        "marker": {"color": colors},
-                    }
-                ],
-                "layout": {
-                    "xaxis": {"automargin": True},
-                    "yaxis": {
-                        "automargin": True,
-                        "title": {"text": column}
-                    },
-                    "height": 250,
-                    "margin": {"t": 10, "l": 10, "r": 10},
-                },
-            },
-        )
-        # check if column exists - user may have deleted it
-        # If `column.deletable=False`, then you don't
-        # need to do this check.
-        for column in ["pop", "lifeExp", "gdpPercap"] if column in dff
-    ]
